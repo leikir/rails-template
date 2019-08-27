cf554f20320f7c1123745c7e40a5b0d03c7b4b4b
Fabien Karquel <fabien.karquel@gmail.com>
2019-08-27 10:52:56 +0200

    chore: html.erb => slim

--- app/template.rb
+++ app/template.rb
@@ -11,7 +11,12 @@ copy_file "app/controllers/home_controller.rb"
 # Helpers
 copy_file "app/helpers/layout_helper.rb"
 copy_file "app/helpers/retina_image_helper.rb"
-copy_file "app/views/layouts/application.html.erb", force: true
-template "app/views/layouts/base.html.erb.tt"
-copy_file "app/views/shared/_flash.html.erb"
-copy_file "app/views/home/index.html.erb"
+
+# Views
+remove_file "app/views/layouts/application.html.erb"
+copy_file "app/views/layouts/application.slim"
+template "app/views/layouts/base.slim.tt"
+remove_file "app/views/shared/_flash.html.erb"
+copy_file "app/views/shared/_flash.slim"
+remove_file "app/views/home/index.html.erb"
+copy_file "app/views/home/index.slim"
