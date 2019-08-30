f37c5552b25342a37174d36e97d5d57bdcaa8fb4
Fabien Karquel <fabien.karquel@gmail.com>
2019-08-30 15:07:34 +0200

    feat: Add Mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,9 +4,12 @@ comment_lines "config/environments/development.rb", mailer_regex
 insert_into_file "config/environments/development.rb", after: mailer_regex do
   <<-RUBY
 
-  # Ensure mailer works in development.  
+  # Ensure mailer works in development.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: ENV['MAILCATCHER_SMTP_ADDRESS'], port: ENV['MAILCATCHER_SMTP_PORT'] }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.asset_host = "http://localhost:3000"
   RUBY
 end
